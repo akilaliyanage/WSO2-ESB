38ca62e1f059c49fdc608b8ce5acb20e6ae1236c
Akila Liyanage <akila.liyanage@madmobile.com>
2021-05-02 20:27:41 +0530

    node aoo

---
+++ back-end/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+
+//Import Routes
+
+//Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+
+//connecting to the database
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {useNewUrlParser: true , useUnifiedTopology:true},
+    () =>{
+        console.log("connected to the database")
+    }
+)
+
+//server start
+app.listen(8000);
--- frontend/src/components/delivery/delivery_admin/OrderTable.jsx
+++ frontend/src/components/delivery/delivery_admin/OrderTable.jsx
@@ -1,54 +1,7 @@
 import React, { Component } from 'react';
 import { Table, Tag, Space, Descriptions, Card } from 'antd';
+import { Drawer, Button } from 'antd';
 
-const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: text => <a>{text}</a>,
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      render: tags => (
-        <>
-          {tags.map(tag => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            if (tag === 'loser') {
-              color = 'volcano';
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <Space size="middle">
-          <a>Invite {record.name}</a>
-          <a>Delete</a>
-        </Space>
-      ),
-    },
-  ];
 
   const data = [
     {
@@ -74,16 +27,92 @@ const columns = [
     },
   ];
 
+
 class OrderTable extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            visible : false, 
+            setVisible : false,
+         }
     }
+
+
+    showDrawer = (record) => {
+        console.log(record)
+        this.setState({visible:true})
+      };
+
+      onClose = () => {
+        this.setState({visible:false})
+      };
     render() { 
+        const columns = [
+            {
+              title: 'Name',
+              dataIndex: 'name',
+              key: 'name',
+              render: text => <a>{text}</a>,
+            },
+            {
+              title: 'Age',
+              dataIndex: 'age',
+              key: 'age',
+            },
+            {
+              title: 'Address',
+              dataIndex: 'address',
+              key: 'address',
+            },
+            {
+              title: 'Tags',
+              key: 'tags',
+              dataIndex: 'tags',
+              render: tags => (
+                <>
+                  {tags.map(tag => {
+                    let color = tag.length > 5 ? 'geekblue' : 'green';
+                    if (tag === 'loser') {
+                      color = 'volcano';
+                    }
+                    return (
+                      <Tag color={color} key={tag}>
+                        {tag.toUpperCase()}
+                      </Tag>
+                    );
+                  })}
+                </>
+              ),
+            },
+            {
+              title: 'Action',
+              key: 'action',
+              render: (text, record) => (
+                <Space size="middle">
+                 <Button type="primary" onClick={() => this.showDrawer(record)}>
+                        Edit Delivery
+                </Button>
+                  <a>Delete</a>
+                </Space>
+              ),
+            },
+          ]
         return ( 
             <Card>
                 <Descriptions title="Current Deliveries"/>
                 <Table columns={columns} dataSource={data} />
+
+                <Drawer
+        title="Basic Drawer"
+        placement="right"
+        closable={false}
+        onClose={this.onClose}
+        visible={this.state.visible}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
             </Card>
          );
     }
